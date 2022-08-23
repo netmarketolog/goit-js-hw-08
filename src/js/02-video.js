6990dbccece09da65e61ec6fc003df8a2b1d83f6
Alexander Shabanov <99245672+netmarketolog@users.noreply.github.com>
2022-08-23 23:20:57 +0300

    add-hw-8.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,11 @@
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
+
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-player.setCurrentTime()
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+player.on('timeupdate', throttle(function(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds)
+}), 1000);
