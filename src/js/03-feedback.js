3f2d209328c68ee8578c7e4dd2daf406516bac0e
Alexander Shabanov <99245672+netmarketolog@users.noreply.github.com>
2022-08-24 19:40:20 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle'
 
+const KEY_LOCALSTORAGE = 'feedback-form-state'
+const inputObject = {}
+
+const refs = {
+    formBox: document.querySelector('.feedback-form'),
+    inputEmail: document.querySelector('input'),
+    textareaMessage: document.querySelector('textarea'),
+    textInMemory: JSON.parse(localStorage.getItem(KEY_LOCALSTORAGE)),
+}
+
+
+
+refs.formBox.addEventListener('submit', onFormSubmit)
+refs.textareaMessage.addEventListener('input', onTextareaSubmit)
+    // refs.formBox.addEventListener('input', throttle(addToLocal, 500))
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+}
+
+function onTextareaSubmit(e) {
+    const mes = e.currentTarget.value
+    localStorage.setItem(KEY_LOCALSTORAGE, mes)
+
+}
